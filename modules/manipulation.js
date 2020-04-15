dabc8a8d8befe84bfdefc54b99cdb3fb89cdb683
Sjors <sjorspieter@live.nl>
2020-04-15 10:47:44 +0200

    test ready

--- modules/manipulation.js
+++ modules/manipulation.js
@@ -2,7 +2,15 @@ function reduce(data, number) {
     const topTen = data.slice(0, number)
 
     topTen.forEach(entry => {
+        // Change date
         entry.day = changeDate(entry.day)
+
+        // Change null into 0
+        Object.entries(entry.cases).forEach(row => {
+            if (row[1] === null) {
+                entry.cases[row[0]] = 0
+            }
+        })
     })
 
     return topTen
